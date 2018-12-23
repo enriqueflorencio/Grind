b06aca3be2d30efe10c6b2f51542f6502ada8d10
Daniel Florencio <dafloren@ucsd.edu>
2018-12-23 00:32:39 -0800

    Initial Commit

---
+++ Grind/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Grind
+//
+//  Created by Daniel Florencio on 12/23/18.
+//  Copyright © 2018 Enrique Florencio. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
